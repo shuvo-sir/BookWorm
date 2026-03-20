0716fe911fe8fda5780d4af64c70a8ec99dea0b9
Shuvo Halder <shuvohalder499@gmail.com>
2026-03-20 13:17:38 +0600

    build the sign up ui some part

--- mobile/app/(auth)/signup.jsx
+++ mobile/app/(auth)/signup.jsx
@@ -1,11 +1,53 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, KeyboardAvoidingView, Platform, TextInput} from 'react-native'
+import styles from '../../assets/styles/signup.styles'
+import { useState } from 'react';
+import Ionicons from "react-native-vector-icons/Ionicons"
+import COLORS from '../../constants/colors';
 
 const signup = () => {
+
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const handleSignUp = () => {};
   return (
-    <View>
-      <Text>signup</Text>
-    </View>
+     <KeyboardAvoidingView
+          style = {{ flex: 1,}}
+          behavior={Platform.OS === "ios" ? "padding" : "height"}
+        >
+        <View style = {styles.container}>
+          <View style = {styles.card}>
+            <View style = {styles.header}>
+              <Text style= {styles.title}>BookWorm🐛</Text>
+              <Text style={styles.subtitle}>Share your favorite read</Text>
+            </View>
+
+
+            <View style = {styles.formContainer}>
+              <View style={styles.inputGroup}>
+                <Text style={styles.label}>Username</Text>
+              </View>
+              <View style = {styles.inputContainer}>
+                <Ionicons style={styles.inputIcons}
+                name='person-outline'
+                size={20}
+                color={COLORS.primary}/>
+                <TextInput
+                  style={styles.input}
+                  placeholder='Shuvo Halder'
+                  placeholderTextColor={COLORS.placeholderText}
+                  value={username}
+                  onChangeText={setUsername}
+                  autoCapitalize='none'
+                />
+              </View>
+            </View>
+          </View>
+        </View>
+    </KeyboardAvoidingView>
   )
 }
 
--- mobile/assets/styles/signup.styles.js
+++ mobile/assets/styles/signup.styles.js
@@ -1,25 +1,118 @@
 // styles/signup.styles.js
 import { StyleSheet } from "react-native";
 import COLORS from "../../constants/colors";
+import { Label } from "@react-navigation/elements";
 
 const styles = StyleSheet.create({
+  // container: {
+  //   flexGrow: 1,
+  //   backgroundColor: COLORS.background,
+  //   padding: 20,
+  //   justifyContent: "center",
+  // },
+  // card: {
+  //   backgroundColor: COLORS.cardBackground,
+  //   borderRadius: 16,
+  //   padding: 24,
+  //   shadowColor: COLORS.black,
+  //   shadowOffset: { width: 0, height: 2 },
+  //   shadowOpacity: 0.1,
+  //   shadowRadius: 8,
+  //   elevation: 4,
+  //   borderWidth: 2,
+  //   borderColor: COLORS.border,
+  // },
+  // header: {
+  //   alignItems: "center",
+  //   marginBottom: 32,
+  // },
+  // title: {
+  //   fontSize: 32,
+  //   fontWeight: "700",
+  //   fontFamily: "JetBrainsMono-Medium",
+  //   color: COLORS.primary,
+  //   marginBottom: 8,
+  // },
+  // subtitle: {
+  //   fontSize: 16,
+  //   color: COLORS.textSecondary,
+  //   textAlign: "center",
+  // },
+  // formContainer: { marginBottom: 16 },
+  // inputGroup: { marginBottom: 20 },
+  // label: {
+  //   fontSize: 14,
+  //   marginBottom: 8,
+  //   color: COLORS.textPrimary,
+  //   fontWeight: "500",
+  // },
+  // inputContainer: {
+  //   flexDirection: "row",
+  //   alignItems: "center",
+  //   backgroundColor: COLORS.inputBackground,
+  //   borderRadius: 12,
+  //   borderWidth: 1,
+  //   borderColor: COLORS.border,
+  //   paddingHorizontal: 12,
+  // },
+  // inputIcon: { marginRight: 10 },
+  // input: {
+  //   flex: 1,
+  //   height: 48,
+  //   color: COLORS.textDark,
+  // },
+  // eyeIcon: { padding: 8 },
+  // button: {
+  //   backgroundColor: COLORS.primary,
+  //   borderRadius: 12,
+  //   height: 50,
+  //   justifyContent: "center",
+  //   alignItems: "center",
+  //   marginTop: 16,
+  //   shadowColor: COLORS.black,
+  //   shadowOffset: { width: 0, height: 2 },
+  //   shadowOpacity: 0.1,
+  //   shadowRadius: 4,
+  //   elevation: 2,
+  // },
+  // buttonText: {
+  //   color: COLORS.white,
+  //   fontSize: 16,
+  //   fontWeight: "600",
+  // },
+  // footer: {
+  //   flexDirection: "row",
+  //   justifyContent: "center",
+  //   marginTop: 24,
+  // },
+  // footerText: {
+  //   color: COLORS.textSecondary,
+  //   marginRight: 5,
+  // },
+  // link: {
+  //   color: COLORS.primary,
+  //   fontWeight: "600",
+  // },
+
   container: {
-    flexGrow: 1,
-    backgroundColor: COLORS.background,
-    padding: 20,
+    flex: 1,
     justifyContent: "center",
+    padding: 20,
+    backgroundColor: COLORS.background
   },
   card: {
     backgroundColor: COLORS.cardBackground,
     borderRadius: 16,
     padding: 24,
+    borderLeftColor: COLORS.primary,
+    borderWidth: 0,
     shadowColor: COLORS.black,
     shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.1,
+    shadowOpacity: 0.7,
     shadowRadius: 8,
     elevation: 4,
-    borderWidth: 2,
-    borderColor: COLORS.border,
+    borderColor: COLORS.primary,
+    borderLeftWidth: 6, 
   },
   header: {
     alignItems: "center",
@@ -27,71 +120,37 @@ const styles = StyleSheet.create({
   },
   title: {
     fontSize: 32,
-    fontWeight: "700",
     fontFamily: "JetBrainsMono-Medium",
+    fontWeight: 700,
     color: COLORS.primary,
     marginBottom: 8,
   },
   subtitle: {
     fontSize: 16,
     color: COLORS.textSecondary,
-    textAlign: "center",
+    textAlign: "center"
   },
-  formContainer: { marginBottom: 16 },
-  inputGroup: { marginBottom: 20 },
+  formContainer: {
+    marginBottom: 16
+  },
+  inputGroup: {
+    marginBottom: 20
+  },
   label: {
     fontSize: 14,
-    marginBottom: 8,
+    fontWeight: 500,
     color: COLORS.textPrimary,
-    fontWeight: "500",
+    marginBottom: 8,
   },
   inputContainer: {
     flexDirection: "row",
-    alignItems: "center",
-    backgroundColor: COLORS.inputBackground,
-    borderRadius: 12,
     borderWidth: 1,
-    borderColor: COLORS.border,
-    paddingHorizontal: 12,
-  },
-  inputIcon: { marginRight: 10 },
-  input: {
-    flex: 1,
-    height: 48,
-    color: COLORS.textDark,
-  },
-  eyeIcon: { padding: 8 },
-  button: {
-    backgroundColor: COLORS.primary,
-    borderRadius: 12,
-    height: 50,
-    justifyContent: "center",
     alignItems: "center",
-    marginTop: 16,
-    shadowColor: COLORS.black,
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.1,
-    shadowRadius: 4,
-    elevation: 2,
-  },
-  buttonText: {
-    color: COLORS.white,
-    fontSize: 16,
-    fontWeight: "600",
-  },
-  footer: {
-    flexDirection: "row",
-    justifyContent: "center",
-    marginTop: 24,
-  },
-  footerText: {
-    color: COLORS.textSecondary,
-    marginRight: 5,
-  },
-  link: {
-    color: COLORS.primary,
-    fontWeight: "600",
-  },
+    paddingHorizontal: 12,
+    borderColor: COLORS.primary,
+    borderRadius: 16,
+    
+  }
 });
 
 export default styles;
