dd05b1eaa540ee8b19bb79f74a11fe21022576b9
Shuvo Halder <shuvohalder499@gmail.com>
2026-03-26 15:53:32 +0600

    add cron

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,10 +4,16 @@ import authRoutes from './routes/authRoutes.js';
 import bookRoutes from './routes/bookRoutes.js';
 import {connectDB} from './lib/db.js';
 import cors from 'cors';
+import job from './lib/cron.js';
+
 
 const app = express();
+
+
 const PORT = process.env.PORT || 3000;
 
+
+job.start();
 // ADD THIS: This allows your app to read req.body
 app.use(express.json()); 
 app.use(cors()); // Enable CORS for all routes
